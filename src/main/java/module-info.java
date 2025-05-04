cba2bdd75496b8561c74b17ec7f837e1a4718ae1
Cerveau70 <cerveau70@192.168.1.4>
2025-05-04 20:05:37 +0000

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.esc.esc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.esc.esc to javafx.fxml;
+    exports org.esc.esc;
+}
